e5345285c081974ebc33ee7387e2f4d80165160a
theraggedflygon <68510342+theraggedflygon@users.noreply.github.com>
2022-09-15 09:34:57 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>BEKC</title>
+        <meta name="description" content="Ben's Enzyme Kinetics Calculator" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Hello World!</div>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Created by Ben Weiner
+        </a>
+      </footer>
+    </div>
+  );
+}
