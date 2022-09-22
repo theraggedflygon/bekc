8a0e1f8501fc4f66fa37d4ba01d7671ff07cbd11
theraggedflygon <68510342+theraggedflygon@users.noreply.github.com>
2022-09-22 14:17:54 -0500

    Trying to figure out deployment

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Header = () => {
   return (
     <div className="flex-none border-b border-black">
-      <div className="text-3xl m-3">Ben's Enzyme Kinetics Calculator</div>
+      <div className="text-3xl m-3">{"Ben's Enzyme Kinetics Calculator"}</div>
     </div>
   );
 };
