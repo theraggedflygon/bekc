b41aae2d578d1bef22c5791b7dc58e662bf46ec0
theraggedflygon <68510342+theraggedflygon@users.noreply.github.com>
2022-09-22 13:51:31 -0500

    More work on the columns modal, adds serial dilution

--- components/columsModal.js
+++ components/columsModal.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const ColumnsModal = ({ headers, setModalShow }) => {
   const [settings, setSettings] = useState({ plotTemp: false, useTime: true });
+  const [serialVals, setSerialVals] = useState({ start: 1000, factor: 2 });
   const [labels, setLabels] = useState([]);
   const [control, setControl] = useState([]);
 
@@ -35,7 +36,6 @@ const ColumnsModal = ({ headers, setModalShow }) => {
   const handleChange = (e, i) => {
     const newLabels = [...labels];
     let newValue = e.target.value;
-    console.log(newValue, newValue === "");
     if (!control[i]) {
       if (isNaN(newValue) || Number.parseFloat(newValue) < 0) {
         return;
@@ -49,6 +49,43 @@ const ColumnsModal = ({ headers, setModalShow }) => {
     setLabels(newLabels);
   };
 
+  const handleRadio = (field, newVal) => {
+    const newSettings = { ...settings };
+    newSettings[field] = newVal;
+    setSettings(newSettings);
+  };
+
+  const handleSerialChange = (e, field) => {
+    const newSerial = { ...serialVals };
+    let newValue = e.target.value;
+    if (isNaN(newValue)) {
+      return;
+    } else if (newValue === "") {
+      newValue = 0;
+    } else {
+      newValue = Number.parseFloat(newValue);
+      if (newValue < 0) {
+        return;
+      }
+    }
+    newSerial[field] = newValue;
+    setSerialVals(newSerial);
+  };
+
+  const applySerial = () => {
+    const { start, factor } = serialVals;
+    console.log(start, factor);
+    let current = start;
+    const newLabels = [...labels];
+    for (let i = 0; i < labels.length; i++) {
+      if (labels[i] === "" && !control[i]) {
+        newLabels[i] = current;
+        current /= factor;
+      }
+    }
+    setLabels(newLabels);
+  };
+
   return (
     <div
       className="bg-black inset-0 absolute bg-opacity-50 flex justify-center"
@@ -56,7 +93,7 @@ const ColumnsModal = ({ headers, setModalShow }) => {
       onClick={(e) => handleClose(e)}
     >
       <div
-        className="max-w-2xl w-full mx-auto pt-20 relative"
+        className="max-w-3xl w-full mx-auto pt-20 relative"
         id="modal-center"
       >
         <div className="bg-white p-6 rounded-lg">
@@ -75,7 +112,10 @@ const ColumnsModal = ({ headers, setModalShow }) => {
                 <tbody>
                   {headers.slice(3).map((colName, idx) => {
                     return (
-                      <tr className="border border-b-1 border-gray-200">
+                      <tr
+                        id={idx}
+                        className="border border-b-1 border-gray-200"
+                      >
                         <td className="border border-l-1 pl-2 border-gray-200">
                           {colName}
                         </td>
@@ -117,11 +157,72 @@ const ColumnsModal = ({ headers, setModalShow }) => {
               <div className="text-center font-bold">Settings</div>
               <div className="pl-2">
                 <div className="font-bold">Graph Temperature?</div>
+                <div className="flex flex-col">
+                  <div className="flex items-center mb-1">
+                    <input
+                      type="radio"
+                      onChange={() => handleRadio("plotTemp", true)}
+                      checked={settings.plotTemp}
+                    ></input>
+                    <label className="pl-2">Yes</label>
+                  </div>
+                  <div className="flex items-center mb-1">
+                    <input
+                      type="radio"
+                      onChange={() => handleRadio("plotTemp", false)}
+                      checked={!settings.plotTemp}
+                    ></input>
+                    <label className="pl-2">No</label>
+                  </div>
+                </div>
 
                 <div className="font-bold">Set Independent Variable:</div>
                 <div className="flex flex-col">
-                  <input type="radio"></input>
-                  <input type="radio"></input>
+                  <div className="flex items-center mb-1">
+                    <input
+                      type="radio"
+                      onChange={() => handleRadio("useTime", true)}
+                      checked={settings.useTime}
+                    ></input>
+                    <label className="pl-2">Time</label>
+                  </div>
+                  <div className="flex items-center mb-1">
+                    <input
+                      type="radio"
+                      onChange={() => handleRadio("useTime", false)}
+                      checked={!settings.useTime}
+                    ></input>
+                    <label className="pl-2">Cycle Number</label>
+                  </div>
+                </div>
+                <div className="font-bold">Add Serial Dilution</div>
+                <div className="flex flex-col">
+                  <div className="flex items-center mb-1">
+                    <div>Start Conc. (μM)</div>
+                    <input
+                      type="text"
+                      className="border border-gray-400 ml-2 w-20"
+                      value={serialVals.start}
+                      onChange={(e) => handleSerialChange(e, "start")}
+                    ></input>
+                  </div>
+                  <div className="flex items-center mb-1">
+                    <div>Dilution Factor</div>
+                    <input
+                      type="text"
+                      className="border border-gray-400 ml-4 w-20"
+                      value={serialVals.factor}
+                      onChange={(e) => handleSerialChange(e, "factor")}
+                    ></input>
+                  </div>
+                  <div className="flex justify-center h-full">
+                    <button
+                      className="bg-green-500 w-1/2 text-white rounded-lg p-2"
+                      onClick={applySerial}
+                    >
+                      Add Serial Dilution
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
