d6cb2cb54a7915e71754dc1e7ceaf61d601c2f9a
theraggedflygon <68510342+theraggedflygon@users.noreply.github.com>
2022-12-13 00:44:15 -0600

    Makes the spinner look like an actual spinner

--- components/spinner.js
+++ components/spinner.js
@@ -1,10 +1,12 @@
 const Spinner = () => {
   return (
-    <div className="flex justify-center items-center">
-      <div
-        className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
-        role="status"
-      ></div>
+    <div className="text-center">
+      <div className="w-full flex justify-center">
+        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-gradient-to-tr from-blue-500 to-blue-200 animate-spin">
+          <div className="h-12 w-12 rounded-full bg-white"></div>
+        </div>
+      </div>
+      <div>Please wait. Your data is loading.</div>
     </div>
   );
 };
